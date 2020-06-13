cd2532beaa42170faf2a974ec649d0f661978832
careychua <careychua@bikerider.com>
2020-06-14 00:04:47 +0800

    Final Commit

--- react/src/index.js
+++ react/src/index.js
@@ -985,56 +985,56 @@
 
 
 /* Dynamically filter array: Array.filter() */
-  class MyComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        users: [
-          {
-            username: 'Jeff',
-            online: true
-          },
-          {
-            username: 'Alan',
-            online: false
-          },
-          {
-            username: 'Mary',
-            online: true
-          },
-          {
-            username: 'Jim',
-            online: false
-          },
-          {
-            username: 'Sara',
-            online: true
-          },
-          {
-            username: 'Laura',
-            online: true
-          }
-        ]
-      }
-    }
-    render() {
-      const usersOnline = this.state.users.filter(item => item.online);
-      const renderOnline = usersOnline.map(item => {
-        let name = item.username;
-        return (
-          <li key={name}>{name}</li>
-        );
-      });
-      return (
-         <div>
-           <h1>Current Online Users:</h1>
-           <ul>
-             {renderOnline}
-           </ul>
-         </div>
-      );
-    }
-  };
+  // class MyComponent extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       users: [
+  //         {
+  //           username: 'Jeff',
+  //           online: true
+  //         },
+  //         {
+  //           username: 'Alan',
+  //           online: false
+  //         },
+  //         {
+  //           username: 'Mary',
+  //           online: true
+  //         },
+  //         {
+  //           username: 'Jim',
+  //           online: false
+  //         },
+  //         {
+  //           username: 'Sara',
+  //           online: true
+  //         },
+  //         {
+  //           username: 'Laura',
+  //           online: true
+  //         }
+  //       ]
+  //     }
+  //   }
+  //   render() {
+  //     const usersOnline = this.state.users.filter(item => item.online);
+  //     const renderOnline = usersOnline.map(item => {
+  //       let name = item.username;
+  //       return (
+  //         <li key={name}>{name}</li>
+  //       );
+  //     });
+  //     return (
+  //        <div>
+  //          <h1>Current Online Users:</h1>
+  //          <ul>
+  //            {renderOnline}
+  //          </ul>
+  //        </div>
+  //     );
+  //   }
+  // };
 
 
 /* Render React on server: renderToString */
