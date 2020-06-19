b35f1bf4c17eb7b8954ee807ea12dbc605b39eb1
careychua <careychua@bikerider.com>
2020-06-19 20:30:53 +0800

    Completed

---
+++ drum/src/js/constants.js
@@ -0,0 +1,157 @@
+let keys = [
+    {
+        key: 'Q',
+        code: 81,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+                        display: 'Heater-1'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+                        display: 'Chord-1'
+                }
+            }
+    },
+    {
+        key: 'W',
+        code: 87,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+                        display: 'Heater-2'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+                        display: 'Chord-2'
+                }
+            }
+    },
+    {
+        key: 'E',
+        code: 69,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+                        display: 'Heater-3'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+                        display: 'Chord-3'
+                }
+            }
+    },
+    {
+        key: 'A',
+        code: 65,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+                        display: 'Heater-4'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+                        display: 'Shaker'
+                }
+            }
+    },
+    {
+        key: 'S',
+        code: 83,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+                        display: 'Clap'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+                        display: 'Open-HH'
+                }
+            }
+    },
+    {
+        key: 'D',
+        code: 68,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+                        display: 'Open-HH'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+                        display: 'Closed-HH'
+                }
+            }
+    },
+    {
+        key: 'Z',
+        code: 90,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+                        display: 'Kick-n\'-Hat'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+                        display: 'Punchy-Kick'
+                }
+            }
+    },
+    {
+        key: 'X',
+        code: 88,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+                        display: 'Kick'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+                        display: 'Side-Stick'
+                }
+            }
+    },
+    {
+        key: 'C',
+        code: 67,
+        bank: {
+                1: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+                        display: 'Closed-HH'
+                }, 
+                2: {
+                        url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+                        display: 'Snare'
+                }
+            }
+    },
+];
+
+let switches =  [
+    {
+        power:  [
+                    {
+                        code: 'O',
+                        value: false, 
+                        display: 'Power OFF'
+                    },
+                    {
+                        code: 'I',
+                        value: true,
+                        display: 'Power ON'
+                    }
+                ],
+        bank:   [
+                    {
+                        code: 1, 
+                        value: false,                        
+                        display: 'Bank 1: Heater Kit'
+                    },
+                    {
+                        code: 2, 
+                        value: true,                        
+                        display: 'Bank 2: Smooth Piano Kit'
+                    }
+                ]
+    }
+];
--- drum/src/js/index.js
+++ drum/src/js/index.js
@@ -1,35 +1,105 @@
+const POWER = 'power';
+const BANK = 'bank';
+const DISPLAY = 'display'
+
 // React
 class DrumMachine extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            power: false
+            power: false,
+            bank: 1,
+            display: ''
         };
 
         this.handleClickAndKeyPress = this.handleClickAndKeyPress.bind(this);
         this.handleKey = this.handleKey.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.clearDisplay = this.clearDisplay.bind(this);
     }
 
     componentDidMount() {
         document.addEventListener('keydown', this.handleClickAndKeyPress);
     }
 
-    handleKey(key) {
-        $(('#').concat(key)).addClass("highlight-key");
+    handleClick(event) {
+        let type = event.target.id;
+        let checked = event.target.checked;
+        
+        switch(type) {
+            case POWER:
+                let display = switches[0][POWER].filter(item => item.value == checked);
+                this.setState({
+                        power: checked,
+                        display: display[0].display
+                });
+
+                if (checked) {
+                    setTimeout(() => {
+                        this.setState(state => {
+                            let display = switches[0][BANK].filter(item => item.code == state.bank);
+                            return ({
+                                display: display[0].display
+                            });                        
+                        });
+                        this.clearDisplay(1500);
+                    }, 1000);
+                }
+                else {
+                    this.clearDisplay(1500);
+                }              
+                break;
+
+            case BANK:
+                this.setState({
+                    bank: checked ? 2 : 1
+                });
+
+                if (this.state.power) {
+                    let display = switches[0][BANK].filter(item => item.value == checked);
+                    this.setState({
+                            display: display[0].display
+                    });
+                }
+                this.clearDisplay(1500);
+                break;
+        }
+    }
+
+    clearDisplay(duration) {
         setTimeout(() => {
-            $(('#').concat(key)).removeClass("highlight-key");
+            this.setState({
+                display: ''                      
+            });
+        }, duration);
+    }
+
+    handleKey(keyObj) {
+        let bank = this.state.bank;
+        this.setState({
+            display: keyObj.bank[bank].display
+        });
+
+        this.clearDisplay(1000);
+
+        $(('#key-').concat(keyObj.key)).addClass("highlight-key");
+
+        setTimeout(() => {
+            $(('#key-').concat(keyObj.key)).removeClass("highlight-key");
         }, 250);
-        document.querySelector(('#audio-').concat(key)).play();
+
+        document.querySelector(('#').concat(keyObj.key)).play();
     }
 
     handleClickAndKeyPress(event) {
         if (this.state.power) {
             let key = event.target.id;
+            key = key.replace("key-", "");
             let code = event.keyCode;
             let keyObj = keys.filter(item => (item.key == key || item.code == code));
             if (keyObj.length > 0) {
-                this.handleKey(keyObj[0].key);
+                this.handleKey(keyObj[0]);
             }
         }
     }
@@ -37,8 +107,8 @@ class DrumMachine extends React.Component {
     render() {
         return (
             <div id="drum-machine">
-                <DrumPad handleClick={ this.handleClickAndKeyPress }/>
-                <DrumControls />
+                <DrumPad bank={ this.state.bank } handleClick={ this.handleClickAndKeyPress }/>
+                <DrumControls handleClick= { this.handleClick } display= { this.state.display }/>
             </div>
         );
     }
@@ -51,10 +121,11 @@ class DrumPad extends React.Component {
 
     render() {
         let keysJsx = [];
+        let bank = this.props.bank
         keys.map(item => {
             keysJsx.push(
-                <button id={ item.key } key={ item.key } onClick={ this.props.handleClick } className="drum-pad">
-                    { item.key.toUpperCase() }<audio id={ ('audio-').concat(item.key) } src={ item.url } className="clip" />
+                <button id={ ("key-").concat(item.key) } key={ item.key } onClick={ this.props.handleClick } className="drum-pad">
+                    { item.key.toUpperCase() }<audio id={ item.key } src={ item.bank[bank].url } className="clip" />
                 </button>
             );
         });
@@ -74,18 +145,73 @@ class DrumControls extends React.Component {
     render() {
         return (
             <div id="drum-controls">
-                <div id="power">
-                    power
-                </div>
-                <div id="display">
-                    display
-                </div>
+                <SwitchComponent type={ POWER } handleClick={ this.props.handleClick }/>
+                <Display display={ this.props.display } />
                 <div id="volume">
                     volume
                 </div>
-                <div id="bank">
-                    bank
+                <SwitchComponent type={ BANK }  handleClick={ this.props.handleClick }/>
+            </div>
+        );
+    }
+}
+
+class Display extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id={ DISPLAY }>
+                <div id={ DISPLAY.concat("-text") }>
+                    { this.props.display }
                 </div>
+            </div>
+        );
+    }
+}
+
+class SwitchComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let type = this.props.type;
+        let text = type;
+        let regex = /[a-z]/;
+        text = type.replace(regex, type.match(regex)[0].toUpperCase());
+        return (
+            <div id={ type }>
+                <div id={ type.concat("-text") } type={ type }>{ text }</div>
+                <div>
+                    { switches[0][type][0].code }<Switch type={ type } handleClick={ this.props.handleClick }/>{ switches[0][type][1].code }
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+class Switch extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let type = this.props.type;
+        return (
+            <div id={ type.concat("-switch") }>
+                <label className="switch">
+                    <input type="checkbox" id={ type } onClick={ this.props.handleClick }/>
+                    { type == POWER &&
+                        <span className="slider round"></span>    
+                    }
+                    { type == BANK &&
+                        <span className="slider-bank round"></span>    
+                    }
+                </label>
             </div>
         );
     }
