df6952931285051398ab56704062db1a29f7920d
careychua <careychua@bikerider.com>
2020-06-23 21:16:11 +0800

    Completed Visual

---
+++ clock/src/js/constants.js
@@ -0,0 +1,29 @@
+/* Constants: Strings */
+const MAIN = 'main';
+const CLOCK = 'clock';
+const HEADER = 'header';
+const VALUE = 'value';
+const VALUES = 'values';
+const BREAK = 'break';
+const SESSION = 'session';
+const TIMER = 'timer';
+const TIME_LEFT = 'time-left';
+const LABEL = 'label';
+const DECREMENT = 'decrement';
+const INCREMENT = 'increment';
+const LENGTH = 'length';
+const CONTROLS = 'controls';
+const ARROW = 'arrow';
+const START_STOP = 'start_stop';
+const RESET = 'reset';
+const CONTAINER = 'container';
+
+
+/* Constants: Characters */
+const DASH = '-';
+const SPACE = ' ';
+
+
+/* Constants: Values */
+const headerText = 'pomodoro clock';
+
---
+++ clock/src/js/index.js
@@ -0,0 +1,114 @@
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+    }   
+
+    render() {
+        return(
+            <div id={ CLOCK }>
+                <Header id={ HEADER }/>
+                <Values id={ VALUES }/>
+                <Timer id={ TIMER }/>
+            </div>
+        );
+    }
+}
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let id = this.props.id;
+        let labelId = id.concat(DASH, LABEL);
+        let headerText = 'var'
+        let controlsId = id.concat(DASH, CONTROLS);
+        let controlsClass = TIMER.concat(DASH, CONTROLS);
+        let containerId = TIME_LEFT.concat(DASH, CONTAINER);
+
+        return (
+            <div id={ TIMER }>
+                <div id={ containerId }>
+                    <Header id={ labelId } headerText={ headerText }/>
+                    <div id={ TIME_LEFT }>
+                        var
+                    </div>
+                    
+                </div>
+                <div id={ controlsId }>
+                    <a className={ controlsClass }>
+                        <i class="fas fa-play"/><i class="fas fa-pause"/>
+                    </a>
+                    <a className={ controlsClass }>
+                        <i class="fas fa-redo-alt"/>
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Values extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let id = this.props.id;
+        return (
+            <div id={ id }>
+                <Value id={ BREAK }/>
+                <Value id={ SESSION }/>
+            </div>
+        );
+    }
+}
+
+class Value extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let id = this.props.id
+        let labelId = id.concat(DASH, LABEL);
+        let labelText = id.concat(SPACE, LENGTH);
+        let controlsId = id.concat(DASH, CONTROLS);
+        let decrementId = id.concat(DASH, DECREMENT);
+        let lengthId = id.concat(DASH, LENGTH);
+        let incrementId = id.concat(DASH, INCREMENT);
+        let timerClass = VALUE.concat(DASH, TIMER);
+        return (
+            <div id={ id } className={ VALUE }>
+                <Header id={ labelId } headerText={ labelText }/>
+                <div id={ controlsId } className={ CONTROLS }>
+                    <a id={ decrementId } className={ARROW}><i class="fas fa-arrow-down"/></a>
+                    <div id={ lengthId } className={ timerClass }>var</div>
+                    <a id={ incrementId } className={ARROW}><i class="fas fa-arrow-up"/></a>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let id = this.props.id;
+        let headerText = this.props.headerText;
+
+        return (
+            <div id= { id }>
+                <h1>
+                    { headerText }
+                </h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Clock/>, document.querySelector(MAIN));
